7674117af8d688b08efddc6e5d726466bdc7d803
권용재 <cjdak0907@naver.com>
2023-05-04 18:47:24 +0900

    4주차 미션

--- 율-권용재/3week/src/App.js
+++ 율-권용재/3week/src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
 import Header from './Header';
 import Page from './page';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="home">
-      <Page/>
+    <div>
+      <div className="home">
+        <Page/>
+      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
