2d83e25e718be28c2a81cfc9dd634cee0ac5c603
leesol <jbkljo@naver.com>
2023-05-15 18:45:09 +0900

    feat: 제거 점선 완성

--- 라라-이솔/6주차/src/App.js
+++ 라라-이솔/6주차/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
   state = {
     input: "",
     todos: [
-      { id: 0, text: "리액트 예제 해보기", checked: false },
-      { id: 0, text: "TO DO LIST 만들기", checked: true },
-      { id: 0, text: "리액트 공부", checked: false },
+      // { id: 0, text: "리액트 예제 해보기", checked: false },
+      // { id: 0, text: "TO DO LIST 만들기", checked: true },
+      // { id: 0, text: "리액트 공부", checked: false },
     ],
   };
 
@@ -40,11 +40,41 @@ class App extends Component {
       this.handleCreate();
     }
   };
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+    const index = todos.findIndex((todo) => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
+
+    const nextTodos = [...todos]; // 배열을 복사
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked,
+    };
+
+    this.setState({
+      todos: nextTodos,
+    });
+  };
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter((todo) => todo.id !== id),
+    });
+  };
 
   render() {
     const { input, todos } = this.state;
-    const { handleChange, handleCreate, handleKeyPress } = this;
-
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove,
+    } = this;
     return (
       <TodoListTemplate
         form={
@@ -56,7 +86,11 @@ class App extends Component {
           />
         }
       >
-        <TodoItemList todos={todos} />
+        <TodoItemList
+          todos={todos}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+        />
       </TodoListTemplate>
     );
   }
